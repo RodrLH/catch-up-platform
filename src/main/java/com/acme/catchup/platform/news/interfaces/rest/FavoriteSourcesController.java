068d80f50c25fb2382a6cd1076d42ccbb77ee6ae
RodrLH <u202212338@upc.edu.pe>
2024-10-10 20:47:47 -0500

    Revert "feat(favorite sources): add get favorite source by id endpoint.+"
    This reverts commit 345951b5d9f68ffebc45df0a625d7dd475ec0396.

--- src/main/java/com/acme/catchup/platform/news/interfaces/rest/FavoriteSourcesController.java
+++ src/main/java/com/acme/catchup/platform/news/interfaces/rest/FavoriteSourcesController.java
@@ -1,9 +1,7 @@
 package com.acme.catchup.platform.news.interfaces.rest;
 
 import com.acme.catchup.platform.news.domain.model.aggregates.FavoriteSource;
-import com.acme.catchup.platform.news.domain.model.queries.GetFavoriteSourceByIdQuery;
 import com.acme.catchup.platform.news.domain.services.FavoriteSourceCommandService;
-import com.acme.catchup.platform.news.domain.services.FavoriteSourceQueryService;
 import com.acme.catchup.platform.news.interfaces.rest.resources.CreateFavoriteSourceResource;
 import com.acme.catchup.platform.news.interfaces.rest.resources.FavoriteSourceResource;
 import com.acme.catchup.platform.news.interfaces.rest.transform.CreateFavoriteSourceCommandFromResourceAssembler;
@@ -19,11 +17,9 @@ import static org.springframework.http.HttpStatus.CREATED;
 @RequestMapping("/api/v1/favorite-sources")
 public class FavoriteSourcesController {
     private final FavoriteSourceCommandService favoriteSourceCommandService;
-    private final FavoriteSourceQueryService favoriteSourceQueryService;
 
-    public FavoriteSourcesController(FavoriteSourceCommandService favoriteSourceCommandService, FavoriteSourceQueryService favoriteSourceQueryService) {
+    public FavoriteSourcesController(FavoriteSourceCommandService favoriteSourceCommandService) {
         this.favoriteSourceCommandService = favoriteSourceCommandService;
-        this.favoriteSourceQueryService = favoriteSourceQueryService;
     }
 
     @PostMapping
@@ -31,12 +27,5 @@ public class FavoriteSourcesController {
         var command = CreateFavoriteSourceCommandFromResourceAssembler.toCommandFromResource(resource);
         Optional<FavoriteSource> favoriteSource = favoriteSourceCommandService.handle(command);
         return favoriteSource.map(source -> new ResponseEntity<>(FavoriteSourceResourceFromEntityAssembler.toResourceFromEntity(source), CREATED)).orElseGet(() -> ResponseEntity.badRequest().build());
-    }
-
-    @GetMapping("{id}")
-    public ResponseEntity<FavoriteSourceResource> getFavoriteSourceById(@PathVariable Long id) {
-        var query = new GetFavoriteSourceByIdQuery(id);
-        Optional<FavoriteSource> favoriteSource = favoriteSourceQueryService.handle(query);
-        return favoriteSource.map(source -> ResponseEntity.ok(FavoriteSourceResourceFromEntityAssembler.toResourceFromEntity(source))).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
